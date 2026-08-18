e8b48683aa52c269ed6999b2a0a1e83154993b76
code-with-bhavya <bhavayagoyal07@gmail.com>
2026-08-18 22:13:30 +0530

    Update start_musify.bat

--- start_musify.bat
+++ start_musify.bat
@@ -1,4 +1,4 @@
-start cmd /k "cd /d C:\Users\bhavy\OneDrive\Desktop\Backup for worst 2\backend && python -m uvicorn main:app --reload --host 0.0.0.0"
-start cmd /k "cd /d C:\Users\bhavy\OneDrive\Desktop\Backup for worst 2\frontend && python -m http.server 3000"
+start cmd /k "cd /d C:\Users\bhavy\OneDrive\Desktop\git musify\backend && python -m uvicorn main:app --reload --port 8000"
+start cmd /k "cd /d C:\Users\bhavy\OneDrive\Desktop\git musify\frontend && python -m http.server 3000"
 timeout /t 5
 start http://localhost:3000
